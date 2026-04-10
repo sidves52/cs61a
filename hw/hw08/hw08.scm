286d0b7c871cbed80cec29b43c85063d189388d7
Sid T <thomas.siddharth@gmail.com>
2026-04-10 16:23:26 -0700

    [hw08] finished

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,7 +1,29 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+    (if (null? s)
+        #t
+        (begin
+        (define (helper l)
+            (cond
+                ((null? (cdr l)) #t)
+                ((> (car l) (car (cdr l))) #f)
+                (else (helper (cdr l)))))
+        (helper s))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (cond
+        ((null? s) s)
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (cond
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+    (if (null? s)
+        s
+        (cons (car s) (no-repeats (filter (lambda (val) (not (= val (car s)))) s)))
+    )
+)
