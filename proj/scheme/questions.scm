a46a86eb254ff4530a8009fd729b7f3d097caa15
Sid T <thomas.siddharth@gmail.com>
2026-04-11 14:02:33 -0700

    [scheme] init

---
+++ proj/scheme/questions.scm
@@ -0,0 +1,39 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cadar x) (car (cdr (car x))))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+;; Problem 13
+;; Returns a list of two-element lists
+(define (enumerate s)
+  ; BEGIN PROBLEM 13
+  'replace-this-line
+  ; END PROBLEM 13
+  )
+
+
+;; Problem 14
+
+;; Return the value for a key in a dictionary list
+(define (get dict key)
+  ; BEGIN PROBLEM 14
+  'replace-this-line
+  ; END PROBLEM 14
+  )
+
+;; Return a dictionary list with a (key value) pair
+(define (set dict key val)
+  ; BEGIN PROBLEM 14
+  'replace-this-line
+  ; END PROBLEM 14
+  )
+
+;; Problem 15
+
+;; implement solution-code
+(define (solution-code problem solution)
+  ; BEGIN PROBLEM 15
+  'replace-this-line
+  ; END PROBLEM 15
+  )
